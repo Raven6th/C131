7cb07cdfbf616c520c54ff3ddae0f576e08fd45a
Raven6th <kevinrafif33@gmail.com>
2023-12-19 19:31:33 +0800

    starting

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CheckOngkirController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,11 +9,15 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('provinces', [CheckOngkirController::class, 'province'])->name('provinces');
+Route::get('cities', [CheckOngkirController::class, 'city'])->name('cities');
+Route::post('check-ongkir', [CheckOngkirController::class, 'checkOngkir'])->name('check-ongkir');
